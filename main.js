b2654a2c315e9d1d6f73acb41caf6714002f475c
Rob Holloway <robbiechicago@gmail.com>
2015-08-15 20:25:50 +0100

    got the console to log when the top row was xxx and therefore a winner.  Woot.

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ var blButton = document.getElementById('button-bl').addEventListener('click',but
 var bcButton = document.getElementById('button-bc').addEventListener('click',button_id);
 var brButton = document.getElementById('button-br').addEventListener('click',button_id);
 
+var tl = document.getElementById('button-tl');
+var tc = document.getElementById('button-tc');
+var tr = document.getElementById('button-tr');
+var ml = document.getElementById('button-ml');
+var mc = document.getElementById('button-mc');
+var mr = document.getElementById('button-mr');
+var bl = document.getElementById('button-bl');
+var bc = document.getElementById('button-bc');
+var br = document.getElementById('button-br');
+
+
 // set up winning line arrays
 // rows
 arrTop = ['top-left', 'top-centre', 'top-right'];
@@ -38,14 +49,22 @@ function ox() {
 };
 
 function button_id () {
-  oxxy = ox();
-  console.log(this.id);
-  this.innerHTML = oxxy;
-  
+  if (this.innerHTML === '-') {
+    oxxy = ox();
+    console.log(this.id);
+    this.innerHTML = oxxy;
+    winChecker();
+  }; 
+};
+
+
+function winChecker() {
+  winTop = tl.innerHTML + tc.innerHTML + tr.innerHTML;
+  console.log(winTop);
+  if (winTop === 'xxx' || winTop === 'ooo') {
+    console.log("Winner!");
+  }
 }
-
-
-
 
 
 
